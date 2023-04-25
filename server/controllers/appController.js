887024bf1655642507d3751a0a21c5a48d90504f
sanju <sanjusv1006@gmail.com>
2023-04-25 22:45:15 +0530

    done with the get user route next is to update the user data

--- server/controllers/appController.js
+++ server/controllers/appController.js
@@ -133,7 +133,31 @@ export async function login(req, res) {
 
 // POST: http://localhost:8080/api/user/example123
 export async function getUser(req, res) {
-    res.json('getUser route')
+    const {username} = req.params;
+    
+    try {
+
+        //if the variable is empty, nothing is passed inside it
+        if(!username) return res.status(501).send({error:"Invalid Username"}); 
+        
+        //if the varialbe contains something
+        UserModel.findOne({username},function(err,user){
+            if(err) return res.status(500).send({err});   //if some error occurs while executing
+            if(!user) return res.status(500).send({error: "Couldn't find the User"});  //if user is not found in the database
+            
+            //destructuring the password from user and saving the rest of the values inside the REST variable 
+            // const {password,...rest} = user; 
+
+            //since the above statement also returned the un-necessary mongoose data so we have to remove it 
+            //to remove it we have taken the data and converted into JSON and assigned to new object
+            const { password, ...rest } = Object.assign({},user.toJSON());         
+            return res.status(200).send(rest);  //if the user is found them return the user
+        })
+
+    } catch (error){
+        return res.status(404).send({error : "Cannot find User Data"})
+    }
+
 }
 
 
